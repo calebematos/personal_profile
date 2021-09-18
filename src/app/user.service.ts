65e873c53f557523dfa1576e553cc73814e37ee4
Lucas Calebe <calebematos@gmail.com>
2021-09-18 17:50:40 -0300

    Update profile

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,11 +6,10 @@ export class UserService {
     name: "Lucas Calebe Matos Lopes",
     jobTitle: "Full Stack Developer",
     aboutMe: `
-    I'm a Senior Software Engineer with over 7 years of experience developing software with high standards and interacting with multiple
-    stakeholders from technical and non-technical backgrounds, ensuring clarity, quality, and continuous delivery.
-    I work with technologies such as Java, Hibernate, SQL, Oracle Databases. I am committed to work and willing to help.
-    Bachelor's Degree in Information Systems.I love develop and study  software development.
-    In my free time I like to play with my puppy(Totti), watch series, read manga and play some games.
+    I'm a Senior Software Engineer with over 7 years of experience developing software with high standards and interacting with multiple 
+    stakeholders from technical and non-technical backgrounds, ensuring clarity, quality, and continuous delivery. 
+    I work with technologies such as Java, Hibernate, SQL, Oracle Databases. I am committed to working and willing to help. 
+    Bachelor's Degree in Information Systems. I love to develop and study software development. In my free time, I like to play with my puppy(Totti), watch series, read manga and play some games.In my free time I like to play with my puppy(Totti), watch series, read manga and play some games.
    `,
     profilePicture: "./assets/profile.jpg",
     location: "Brazil",
@@ -19,7 +18,7 @@ export class UserService {
     facebookAccount: "facebook.com/calebe.matos.5",
     facebookAddress: "http://facebook.com/calebe.matos.5",
     githubAccount: "github.com/calebematos",
-    githubAddress: "http://github.com/calebematos",
+    githubAddress: "http://www.github.com/calebematos",
     googleAccount: "",
     linkedinAccount: "linkedin.com/in/calebematos",
     linkedinAddress: "http://linkedin.com/in/calebematos",
