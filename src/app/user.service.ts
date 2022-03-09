85fcd7cfa7ae2cf7f20c09b91f0769178734d8ba
Lucas Calebe <calebematos@gmail.com>
2022-03-09 09:33:41 -0300

    Update profile info

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class UserService {
@@ -6,10 +6,12 @@ export class UserService {
     name: "Lucas Calebe Matos Lopes",
     jobTitle: "Full Stack Developer",
     aboutMe: `
-    I'm a Senior Software Engineer with over 7 years of experience developing software with high standards and interacting with multiple 
+    I'm a Senior Software Engineer with over 8 years of experience developing software with high standards and interacting with multiple 
     stakeholders from technical and non-technical backgrounds, ensuring clarity, quality, and continuous delivery. 
     I work with technologies such as Java, Hibernate, SQL, Oracle Databases. I am committed to working and willing to help. 
-    Bachelor's Degree in Information Systems. I love to develop and study software development. In my free time, I like to play with my puppy(Totti), watch series, read manga and play some games.In my free time I like to play with my puppy(Totti), watch series, read manga and play some games.
+    Bachelor's Degree in Information Systems. I love to develop and study software development. In my free time, 
+    I stay with my little baby, playing with her. 
+    When she is sleeping I take the opportunity to play with my puppy(Totti), watch series, read manga and play some games.
    `,
     profilePicture: "./assets/profile.jpg",
     location: "Brazil",
@@ -24,11 +26,10 @@ export class UserService {
     linkedinAddress: "http://linkedin.com/in/lucascalebematos",
     twitterAccount: "twitter.com/calebematos",
     twitterAddress: "http://twitter.com/calebematos",
-    otherAccount: ""
+    otherAccount: "",
   };
 
   getUserInfo() {
     return this.userInfo;
   }
-
 }
